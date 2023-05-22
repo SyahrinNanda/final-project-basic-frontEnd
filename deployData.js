93aa5e39a9260dff8fa5d639da46b0bc0a7fa1f5
SyahrinNanda <syahrinnanda@gmail.com>
2023-05-22 18:10:23 +0800

    Menambahkan File Final Project Basic Fornt End

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = "As'syahrin Nanda"; // TODO: replace this
+
+const studentId = "FE5130073"; // TODO: replace this
+
+const NetlifyDeployUrl = "https://charming-crostata-126f18.netlify.app"; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
